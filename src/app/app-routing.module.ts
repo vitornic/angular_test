d1d7edf5de16c847f24820b4476a12fe92497229
João Vitor Nicolette <clauzer75@hotmail.com>
2022-01-11 19:27:33 -0300

    Add Estrutura Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevelopersComponent } from './pages/developers/developers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DevelopersComponent },
+  { path: 'app', component: DevelopersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/models/developers.ts
@@ -0,0 +1,6 @@
+export interface IDevelopers {
+  createdAt: string;
+  name: string;
+  avatar: string;
+  id: string;
+}
